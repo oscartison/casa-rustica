0a325e550619bac5d84b7259057ff6ce7d476146
Oscar Tison <oscar.tison@codifly.be>
2024-02-08 20:15:27 +0100

    fix duplicate picture

--- app/data/images.ts
+++ app/data/images.ts
@@ -9,6 +9,8 @@ import Image8 from "../../app/images/house/8.jpeg";
 import Image8a from "../../app/images/house/8a.jpeg";
 import Image8b from "../../app/images/house/8b.jpeg";
 import Image9 from "../../app/images/house/9.jpeg";
+import Image9a from "../../app/images/house/9a.jpeg";
+
 import Image10 from "../../app/images/house/10.jpeg";
 import Image11 from "../../app/images/house/11.jpeg";
 import Image12 from "../../app/images/house/12.jpeg";
@@ -16,7 +18,7 @@ import Image13 from "../../app/images/house/13.jpeg";
 import Image14 from "../../app/images/house/14.jpeg";
 import Image15 from "../../app/images/house/15.jpeg";
 import Image17 from "../../app/images/house/17.jpeg";
-import Image17a from "../../app/images/house/17.jpeg";
+import Image17a from "../../app/images/house/17a.jpeg";
 import Image18a from "../../app/images/house/18a.jpeg";
 import Image18 from "../../app/images/house/18.jpeg";
 import Image19 from "../../app/images/house/19.jpeg";
@@ -65,7 +67,11 @@ export const images = [
     {
       original: Image9.src,
       thumbnail: Image9.src
-    }
+    },
+    {
+        original: Image9a.src,
+        thumbnail: Image9a.src
+      }
   ];
   
 export const roomImages = [
